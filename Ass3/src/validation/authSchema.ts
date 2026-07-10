e5a01588864bc15fa77f6f54a9bcc9834544ff89
reinerjustin <reinerjustin.realica@edu.sait.ca>
2026-07-10 01:29:37 -0600

    updated authschema

--- Ass3/src/validation/authSchema.ts
+++ Ass3/src/validation/authSchema.ts
@@ -5,9 +5,13 @@ export const signInSchema = Yup.object({
         .email("Enter a valid email address")
         .required("Email is required"),
 
-    password: Yup.string()
+        password: Yup.string()
         .required("Password is required")
-        .min(8, "Password must be at least 8 characters"),
+        .min(8, "Password must be at least 8 characters")
+        .matches(/[a-z]/, "Password must contain at least one lowercase letter")
+        .matches(/[A-Z]/, "Password must contain at least one uppercase letter")
+        .matches(/\d/, "Password must contain at least one number")
+        .matches(/[^a-zA-Z0-9]/, "Password must contain at least one special character"),
 });
 
 export const signUpSchema = Yup.object({
