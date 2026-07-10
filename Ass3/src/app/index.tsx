c52c3504637faf7ed6e96d14705999051c0e5d1d
reinerjustin <reinerjustin.realica@edu.sait.ca>
2026-07-10 00:07:09 -0600

    updated index

--- Ass3/src/app/index.tsx
+++ Ass3/src/app/index.tsx
@@ -1,17 +1,23 @@
-import { Text, View, StyleSheet } from "react-native";
+import { View, Button } from "react-native";
+import { router } from "expo-router";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+    <View>
+      <Button
+        title="Sign In"
+        onPress={() => router.push("/signin")}
+      />
+
+      <Button
+        title="Sign Up"
+        onPress={() => router.push("/signup")}
+      />
+
+      <Button
+        title="Employee Form"
+        onPress={() => router.push("/employee")}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
