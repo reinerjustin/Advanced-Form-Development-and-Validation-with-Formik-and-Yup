7d0702143709c05dd72a560988e946c47ccbb1fa
Marc Edison Estaca <mebestaca@gmail.com>
2026-07-11 15:33:15 -0600

    added safeareaview

--- Ass3/src/app/index.tsx
+++ Ass3/src/app/index.tsx
@@ -1,19 +1,21 @@
 import { View, Button } from "react-native";
 import { router } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View>
-      <Button
-        title="Sign In"
-        onPress={() => router.push("/signin")}
-      />
+    <SafeAreaView>
+      <View>
+        <Button
+          title="Sign In"
+          onPress={() => router.push("/signin")}
+        />
 
-      <Button
-        title="Sign Up"
-        onPress={() => router.push("/signup")}
-      />
-      
-    </View>
+        <Button
+          title="Sign Up"
+          onPress={() => router.push("/signup")}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
